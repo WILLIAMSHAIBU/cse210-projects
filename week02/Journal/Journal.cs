dc852fa7d0a0cd72ae330dd3ef8f66172b127ed1
WILLIAMSHAIBU <cen-01-12-20@unilia.ac.mw>
2025-05-17 14:26:58 +0200

    Update Journal.cs

--- week02/Journal/Journal.cs
+++ week02/Journal/Journal.cs
@@ -1,1 +1,56 @@
 
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+public class Journal
+{
+    private List<Entry> entries = new List<Entry>();
+    private List<string> prompts = new List<string>
+    {
+        "Who was the most interesting person I interacted with today?",
+        "What was the best part of my day?",
+        "How did I see the hand of the Lord in my life today?",
+        "What was the strongest emotion I felt today?",
+        "If I had one thing I could do over today, what would it be?"
+    };
+
+    public void WriteEntry()
+    {
+        Random random = new Random();
+        string prompt = prompts[random.Next(prompts.Count)];
+        Console.WriteLine(prompt);
+        string response = Console.ReadLine();
+        string date = DateTime.Now.ToShortDateString();
+        entries.Add(new Entry(date, prompt, response));
+    }
+
+    public void DisplayJournal()
+    {
+        foreach (Entry entry in entries)
+        {
+            Console.WriteLine(entry.Display());
+        }
+    }
+
+    public void SaveToFile(string filename)
+    {
+        using (StreamWriter writer = new StreamWriter(filename))
+        {
+            foreach (Entry entry in entries)
+            {
+                writer.WriteLine(entry.ToFileFormat());
+            }
+        }
+    }
+
+    public void LoadFromFile(string filename)
+    {
+        entries.Clear();
+        string[] lines = File.ReadAllLines(filename);
+        foreach (string line in lines)
+        {
+            entries.Add(Entry.FromFileFormat(line));
+        }
+    }
+}
